0bfb276c408526d67eec0e85f7b655edc1e88e4e
lolagarcia5 <dowap14@gmail.com>
2023-06-26 10:56:54 +0200

    listado y filtroerror

--- src/components/App.js
+++ src/components/App.js
@@ -1,85 +1,59 @@
-import {useState} from 'react';
-import '../styles/main.scss';
+import '../styles/components/App.scss';
+import { useEffect, useState } from 'react';
+
+const handleInputSearch = (ev)=>{
+  SetFraseSearch (ev.target.value);
+};
+
+const handleInputSearch = (ev)=>{
+  SetcharacterSearch (ev.target.value);
+};
+
 
 
 function App() {
-  const [numberOfErrors, setNumberOfError] = useState (0);
-  const [lastLetter, setLastLetter] = useState('');
-  
-  const handleClick = (event) => {
-    setNumberOfError(numberOfErrors+1);
-  }
+  const [ frasesList, setFrasesList] =useState ([]);
+useEffect(() => {
+  fetch('https://beta.adalab.es/curso-intensivo-fullstack-recursos/apis/quotes-friends-tv-v1/quotes.json')
+  .then((response) => response.json())
+      .then((data) => {
+        const frases = data.map((eachFrases)=>{
+          return {
+            Frases : eachFrases.frases,
+            character : eachFrases.character
+          }
+        });
+        setFrasesList(frases);
+        });
+      },[]);
+  /* RETURN --> HTML */
+  const renderfrasesList = () => {
+    const filteredList = frasesList.filter()
 
-  const handleLastLetter = (event) => {
-  setLastLetter(event.target.value);
+  return frasesList.map((eachFrases)=>(
+    <li key={eachFrases.frases}>
+      <div className='quotes'>
+      <p>{eachFrases.frases}</p><p className='name'>-{eachFrases.character}</p></div>
+    </li>
+  ))};
+  return(
+  <body>
+  <h1>Frases de Friends</h1>
+  <form>
+  <label>Filtrar por frase</label>
+  <input type= 'text' name ='' placeholder='' autoCompete='off' value={fraseSearch} onInput={handleInputSearch}/>
+  <label>Filtrar por personaje</label>
+  <input type= 'text' name ='' placeholder='' value={characterSearch} onInput={handleInputSearch}/>
+  </form>
+  <ul>
+    <li>
+      <p>{renderfrasesList()}</p>
+    </li>
+  </ul>
+  </body>
+);}
 
-  };
-
-  return (
-    <div className="page">
-      <header>
-        <h1 className="header__title">Juego del ahorcado</h1>
-      </header>
-      <main className="main">
-        <section>
-          <div className="solution">
-            <h2 className="title">Solución:</h2>
-            <ul className="letters">
-              <li className="letter">k</li>
-              <li className="letter">a</li>
-              <li className="letter"></li>
-              <li className="letter">a</li>
-              <li className="letter">k</li>
-              <li className="letter">r</li>
-              <li className="letter"></li>
-              <li className="letter">k</li>
-              <li className="letter">e</li>
-              <li className="letter">r</li>
-            </ul>
-          </div>
-          <div className="error">
-            <h2 className="title">Letras falladas:</h2>
-            <ul className="letters">
-              <li className="letter">f</li>
-              <li className="letter">q</li>
-              <li className="letter">h</li>
-              <li className="letter">p</li>
-              <li className="letter">x</li>
-            </ul>
-          </div>
-          <form className="form">
-            <label className="title" htmlFor="last-letter">Escribe una letra:</label>
-            <input
-              autoComplete="off"
-              className="form__input"
-              maxLength="1"
-              type="text"
-              name="last-letter"
-              id="last-letter"
-              value={lastLetter}
-              onChange= {handleLastLetter}
-            />
-          </form>
-          <button onClick={handleClick} className='sumbtn'>Incrementar</button>
-        </section>
-        <section className={`dummy error-${numberOfErrors}`} >
-          <span className="error-13 eye"></span>
-          <span className="error-12 eye"></span>
-          <span className="error-11 line"></span>
-          <span className="error-10 line"></span>
-          <span className="error-9 line"></span>
-          <span className="error-8 line"></span>
-          <span className="error-7 line"></span>
-          <span className="error-6 head"></span>
-          <span className="error-5 line"></span>
-          <span className="error-4 line"></span>
-          <span className="error-3 line"></span>
-          <span className="error-2 line"></span>
-          <span className="error-1 line"></span>
-        </section>
-      </main>
-    </div>
-  );
-}
+const [fraseSearch, SetfraseSearch] = useState('');
+const [characterSearch, SetcharacterSearch] = useState('');
 
 export default App;
